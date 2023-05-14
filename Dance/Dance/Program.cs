18771b121e30e73d5d9d14815032c5e20b3aba6d
Daniel <dani2014games@gmail.com>
2023-05-14 00:46:34 -0300

    First commit

---
+++ Dance/Dance/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Taking values
+            string name1 = Console.ReadLine();    
+            int points1 = Convert.ToInt32(Console.ReadLine());
+            string name2 = Console.ReadLine();
+            int points2 = Convert.ToInt32(Console.ReadLine());
+
+            DancerPoints dancer1 = new DancerPoints(name1, points1);
+            DancerPoints dancer2 = new DancerPoints(name2, points2);
+
+            //Printing values
+            DancerPoints total = dancer1 + dancer2;
+            Console.WriteLine(total.name);
+            Console.WriteLine(total.points);
+        }
+    }
+
+    //Creating a class
+    class DancerPoints
+    {
+        public string name;
+        public int points;
+        public DancerPoints(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+        //overload the + operator
+
+        public static DancerPoints operator +(DancerPoints a, DancerPoints b)
+        {
+            string n = a.name + " & " + b.name;
+            int p = a.points + b.points;
+            DancerPoints res = new DancerPoints(n, p);
+            return res;
+        }
+    }
+}
---
+++ Words/Words/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {   //List of words
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();  //Input
+
+            bool found = false;
+
+            //your code goes here
+
+            foreach (string n in words)  //Enter inside of the array
+            {
+                if (n.Contains(letter))   //Verify if letter contains
+                {
+                    Console.WriteLine(n);
+                    found = true;
+                }
+            }
+
+            if (found == false)
+            {
+                Console.WriteLine("No match");
+            }
+
+
+        }
+    }
+}
