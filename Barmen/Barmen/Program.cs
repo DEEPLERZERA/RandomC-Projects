e68150b6205787947c7d5a03fed314bdb2cdffdc
Daniel <dani2014games@gmail.com>
2023-05-15 16:57:21 -0300

    Adding Barmen program

---
+++ Barmen/Barmen/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int bebidas = Convert.ToInt32(Console.ReadLine());  //Pega input
+                int mesas = Convert.ToInt32(Console.ReadLine());
+                int z = bebidas / mesas;  //Calcula
+                Console.WriteLine(z);
+
+            }
+            catch (DivideByZeroException e)  //Trata erro
+            {
+                Console.WriteLine("Pelo menos uma mesa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Por favor coloque um inteiro!");
+            }
+        }
+    }
+}
