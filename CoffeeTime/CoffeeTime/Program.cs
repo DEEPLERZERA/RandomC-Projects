6d986e68a48b0c38228f99a4c059d633fc4dcf28
Daniel <dani2014games@gmail.com>
2023-05-17 15:04:16 -0300

    Adding Coffee program

---
+++ CoffeeTime/CoffeeTime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discount = Convert.ToInt32(Console.ReadLine());  //Declara desconto
+
+            Dictionary<string, double> coffee = new Dictionary<string, double>();  //Criando dicionário que vai receber dois parâmetros
+            coffee.Add("Americano", 50);  //Adicionando itens
+            coffee.Add("Latte", 70);
+            coffee.Add("Flat White", 60);
+            coffee.Add("Espresso", 60);
+            coffee.Add("Cappuccino", 80);
+            coffee.Add("Mocha", 90);
+
+            Dictionary<string, double> updatedPrices = new Dictionary<string, double>();  //Criando outro dicionário com preços atualizados
+
+        
+            foreach (var item in coffee)  //Percorrendo array coffee
+            {
+                double discountedPrice = item.Value - discount;  //Gerando desconto
+                updatedPrices[item.Key] = discountedPrice;
+            }
+
+     
+            foreach (var item in updatedPrices)     //Percorrendo array com preços atualizados
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
